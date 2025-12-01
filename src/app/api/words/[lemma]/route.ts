303549b54d834a4b8be350ff376ef50829906cc9
Martín Bravo <123977407+MartinEBravo@users.noreply.github.com>
2025-12-02 01:09:25 +0400

    fix(variant )

--- src/app/api/words/[lemma]/route.ts
+++ src/app/api/words/[lemma]/route.ts
@@ -217,6 +217,7 @@ export async function POST(request: NextRequest) {
               dictionary: 'duech',
               remission: null,
               observation: null,
+              variant: null,
               ...createEmptyMarkerValues(),
             },
           ];
